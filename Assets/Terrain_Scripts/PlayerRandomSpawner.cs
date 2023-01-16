12ca843feba96a315978816c98f19bd58f96749e
russellcachia <rusl.cachia@gmail.com>
2023-01-16 10:52:47 +0100

    submitted

--- Assets/Terrain_Scripts/PlayerRandomSpawner.cs
+++ Assets/Terrain_Scripts/PlayerRandomSpawner.cs
@@ -17,7 +17,7 @@ public class PlayerRandomSpawner : MonoBehaviour
         path.GetComponent<Cube>().SetCubeSize(2, 2, 400);
         path.AddComponent<BoxCollider>();
         path.GetComponent<BoxCollider>().size = new Vector3(4, 4, 800);
-        path.transform.position = new Vector3(Random.Range(50, 900), 450, 500);
+        path.transform.position = new Vector3(Random.Range(50, 900), 500, 500);
         player.transform.position =  path.transform.position + new Vector3(0, 2, 0);
         player.SetActive(true);
     }
